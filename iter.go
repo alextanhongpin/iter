d3891dd7c31c6c89f77405bb451ccde6636fb8d0
alextanhongpin <alextan220990@gmail.com>
2022-04-27 00:47:23 +0800

    chore: add index to map method

--- iter.go
+++ iter.go
@@ -4,22 +4,26 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
-func Map[T any, R any](ts []T, fn func(T) R) []R {
+func Map[T any, R any](ts []T, fn func(T, int) R) []R {
 	res := make([]R, len(ts))
 	for i, t := range ts {
-		res[i] = fn(t)
+		res[i] = fn(t, i)
 	}
+
 	return res
 }
 
 func Filter[T any](ts []T, fn func(T) bool) []T {
-	var res []T
+	res := make([]T, 0, len(ts))
+
 	for _, t := range ts {
 		if !fn(t) {
 			continue
 		}
+
 		res = append(res, t)
 	}
+
 	return res
 }
 
@@ -27,5 +31,6 @@ func Reduce[T any, R constraints.Ordered](ts []T, r R, fn func(T, R, int) R) R {
 	for i, t := range ts {
 		r = fn(t, r, i)
 	}
+
 	return r
 }
