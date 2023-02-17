07d2023181e5d4b8248a3f284f5d58024d6b931e
alextanhongpin <alextan220990@gmail.com>
2023-02-17 13:18:05 +0800

    chore: update lib

--- iter.go
+++ iter.go
@@ -1,36 +1,202 @@
 package iter
 
 import (
-	"golang.org/x/exp/constraints"
+	"sync"
 )
 
-func Map[T any, R any](ts []T, fn func(T, int) R) []R {
-	res := make([]R, len(ts))
-	for i, t := range ts {
-		res[i] = fn(t, i)
+type Result[T any] struct {
+	Data  T
+	Error error
+}
+
+func MapIndex[T any, V any](ts []T, fn func(int) V) []V {
+	res := make([]V, len(ts))
+	for i := 0; i < len(ts); i++ {
+		res[i] = fn(i)
+	}
+
+	return res
+}
+
+func Map[T any, V any](ts []T, fn func(T) V) []V {
+	return MapIndex(ts, func(i int) V {
+		return fn(ts[i])
+	})
+}
+
+// MapError maps T to V, and returns on first error.
+func MapError[T any, V any](ts []T, fn func(T) (V, error)) ([]V, error) {
+	res := make([]V, len(ts))
+
+	for i := range ts {
+		v, err := fn(ts[i])
+		if err != nil {
+			return nil, err
+		}
+		res[i] = v
+	}
+
+	return res, nil
+}
+
+// MapResult maps each item into Result type.
+func MapResult[T any, V any](ts []T, fn func(T) (V, error)) []Result[V] {
+	res := make([]Result[V], len(ts))
+
+	for i := range ts {
+		v, err := fn(ts[i])
+		if err != nil {
+			res[i] = Result[V]{Error: err}
+		} else {
+			res[i] = Result[V]{Data: v}
+		}
+	}
+
+	return res
+}
+
+func EachIndex[T any](ts []T, fn func(int)) {
+	for i := 0; i < len(ts); i++ {
+		fn(i)
+	}
+}
+
+func Each[T any](ts []T, fn func(T)) {
+	EachIndex(ts, func(i int) {
+		fn(ts[i])
+	})
+}
+
+func GoEachIndex[T any](ts []T, fn func(int)) {
+	var wg sync.WaitGroup
+	wg.Add(len(ts))
+
+	for i := 0; i < len(ts); i++ {
+		go func(i int) {
+			defer wg.Done()
+
+			fn(i)
+		}(i)
+	}
+
+	wg.Wait()
+}
+
+func GoEach[T any](ts []T, fn func(T)) {
+	GoEachIndex(ts, func(i int) {
+		fn(ts[i])
+	})
+}
+
+func GoMapIndex[T any, V any](ts []T, fn func(int) V) []V {
+	res := make([]V, len(ts))
+
+	var wg sync.WaitGroup
+	wg.Add(len(ts))
+
+	for i := 0; i < len(ts); i++ {
+		go func(i int) {
+			defer wg.Done()
+
+			res[i] = fn(i)
+		}(i)
+	}
+	wg.Wait()
+
+	return res
+}
+
+func GoMap[T any, V any](ts []T, fn func(T) V) []V {
+	return GoMapIndex(ts, func(i int) V {
+		return fn(ts[i])
+	})
+}
+
+func GoMapIndexFlat[T any, V any](ts []T, fn func(int) []V) []V {
+	res := make([][]V, len(ts))
+
+	var wg sync.WaitGroup
+	wg.Add(len(ts))
+
+	for i := range ts {
+		go func(i int) {
+			defer wg.Done()
+
+			res[i] = fn(i)
+		}(i)
+	}
+	wg.Wait()
+
+	return Flat(res)
+}
+
+func GoMapFlat[T any, V any](ts []T, fn func(T) []V) []V {
+	return GoMapIndexFlat(ts, func(i int) []V {
+		return fn(ts[i])
+	})
+}
+
+func GoMapResult[T any, V any](ts []T, fn func(T) (V, error)) []Result[V] {
+	res := make([]Result[V], len(ts))
+
+	var wg sync.WaitGroup
+	wg.Add(len(ts))
+
+	for i := range ts {
+		go func(i int) {
+			defer wg.Done()
+
+			v, err := fn(ts[i])
+			if err != nil {
+				res[i] = Result[V]{Error: err}
+			} else {
+				res[i] = Result[V]{Data: v}
+			}
+		}(i)
+	}
+	wg.Wait()
+
+	return res
+}
+
+func FilterIndex[T any](ts []T, fn func(int) bool) []T {
+	res := make([]T, 0, len(ts))
+
+	for i := 0; i < len(ts); i++ {
+		if !fn(i) {
+			continue
+		}
+
+		res = append(res, ts[i])
 	}
 
 	return res
 }
 
 func Filter[T any](ts []T, fn func(T) bool) []T {
-	res := make([]T, 0, len(ts))
-
-	for _, t := range ts {
-		if !fn(t) {
-			continue
-		}
-
-		res = append(res, t)
-	}
-
-	return res
+	return FilterIndex(ts, func(i int) bool {
+		return fn(ts[i])
+	})
 }
 
-func Reduce[T any, R constraints.Ordered](ts []T, r R, fn func(T, R, int) R) R {
-	for i, t := range ts {
-		r = fn(t, r, i)
+func ReduceIndex[T, V any](ts []T, r V, fn func(V, int) V) V {
+	for i := 0; i < len(ts); i++ {
+		r = fn(r, i)
 	}
 
 	return r
+}
+
+func Reduce[T, V any](ts []T, r V, fn func(V, T) V) V {
+	return ReduceIndex(ts, r, func(v V, i int) V {
+		return fn(v, ts[i])
+	})
+}
+
+func Flat[T any](ts [][]T) []T {
+	var res []T
+	for _, t := range ts {
+		res = append(res, t...)
+	}
+	return res
 }
---
+++ iter_test.go
@@ -0,0 +1,93 @@
+package iter_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/alextanhongpin/iter"
+)
+
+func TestReduce(t *testing.T) {
+	got := iter.Reduce([]int{1, 2, 3}, 0, func(acc, n int) int {
+		return acc + n
+	})
+	if exp := 6; exp != got {
+		t.Errorf("expected %d, got %d", exp, got)
+	}
+}
+
+func TestReduceIndex(t *testing.T) {
+	chars := []string{"a", "b", "c"}
+	got := iter.ReduceIndex(chars, "", func(acc string, i int) string {
+		return acc + chars[i]
+	})
+	if exp := "abc"; exp != got {
+		t.Errorf("expected %s, got %s", exp, got)
+	}
+}
+
+type post struct {
+	id     int
+	photos []*photo
+}
+
+type photo struct {
+	id     int
+	postID int
+	tags   []tag
+}
+
+type tag struct {
+	id      int
+	photoID int
+	name    string
+}
+
+func TestLazyLoading(t *testing.T) {
+	start := time.Now()
+	posts := []post{{id: 1}, {id: 2}, {id: 3}}
+
+	fetchPhotos := func(p post) []*photo {
+		photos := make([]*photo, 3)
+		for i := 0; i < 3; i++ {
+			photos[i] = &photo{id: p.id * i, postID: p.id}
+		}
+		time.Sleep(1 * time.Second)
+		return photos
+	}
+
+	fetchTags := func(p *photo) []tag {
+		tags := make([]tag, 5)
+		for i := 0; i < 5; i++ {
+			tags[i] = tag{id: p.id * i, photoID: p.id}
+		}
+		time.Sleep(1 * time.Second)
+		return tags
+	}
+
+	// Wait for all photos to fetch.
+	photos := iter.GoMapIndexFlat(posts, func(i int) []*photo {
+		photos := fetchPhotos(posts[i])
+		posts[i].photos = photos
+		return photos
+	})
+
+	// Wait for all tags to fetch.
+	iter.GoEachIndex(photos, func(j int) {
+		tags := fetchTags(photos[j])
+		photos[j].tags = tags
+		fmt.Println("done fetching tags", j)
+	})
+
+	t.Log(time.Since(start))
+	for i := range posts {
+		t.Logf("%d: post id: %+v\n", i+1, posts[i].id)
+		t.Log(len(posts[i].photos), "photos")
+
+		for j := range posts[i].photos {
+			t.Logf("\t%+v\n", posts[i].photos[j])
+		}
+		fmt.Println()
+	}
+}
